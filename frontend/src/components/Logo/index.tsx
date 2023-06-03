85e3c9564a64fece6b95872e1dc1fd033cd1f52a
Kim Yoo <kimyoo04eco@naver.com>
2023-06-04 02:34:14 +0900

    fix: Logo 컴포넌트 텍스트 수정

--- frontend/src/components/Logo/index.tsx
+++ frontend/src/components/Logo/index.tsx
@@ -1,14 +1,9 @@
 import Link from "next/link";
-import ButtonWrapper from "@components/Animation/ButtonWrapper";
 
 export default function Logo() {
   return (
-    <Link href={"/"}>
-      <ButtonWrapper>
-        <span className="pb-[6px] pl-[4px] text-2xl font-bold text-main_color_yellow dark:text-main_color_yellow">
-          수어 번역 서비스
-        </span>
-      </ButtonWrapper>
+    <Link href="/">
+      <h1 className="undraggable text-2xl font-bold">Dancify</h1>
     </Link>
   );
 }
