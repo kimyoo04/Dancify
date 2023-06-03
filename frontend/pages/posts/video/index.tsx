a926c9595b0abbd3031c7bb0d26e5983740381de
Kim Yoo <kimyoo04eco@naver.com>
2023-06-04 02:54:05 +0900

    fix: VideoPostsPage 컴포넌트 임시 import 에러 해결

--- frontend/pages/posts/video/index.tsx
+++ frontend/pages/posts/video/index.tsx
@@ -1,10 +1,5 @@
 import MainLayout from "@layouts/MainLayout";
-import VideoPosts from "@scenes/Posts/Video";
 
 export default function VideoPostsPage() {
-  return (
-    <MainLayout>
-      <VideoPosts />
-    </MainLayout>
-  );
+  return <MainLayout>ss</MainLayout>;
 }
