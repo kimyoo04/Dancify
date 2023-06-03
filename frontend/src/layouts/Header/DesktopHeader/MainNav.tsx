d260db29ff9b5a9cee910c52af767d60520cf6d8
Kim Yoo <kimyoo04eco@naver.com>
2023-06-04 02:32:39 +0900

    feat: DesktopHeader 컴포넌트 생성

---
+++ frontend/src/layouts/Header/DesktopHeader/MainNav.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+import { cn } from "@lib/utils";
+
+export function MainNav({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) {
+  return (
+    <nav
+      className={cn(
+        "mx-6 mt-1 flex items-center space-x-4 lg:space-x-6",
+        className
+      )}
+      {...props}
+    >
+      <Link
+        href="/"
+        className="text-sm font-medium transition-colors hover:text-primary"
+      >
+        Home
+      </Link>
+      <Link
+        href="/posts"
+        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      >
+        posts
+      </Link>
+    </nav>
+  );
+}
--- frontend/src/layouts/Header/DesktopHeader/index.tsx
+++ frontend/src/layouts/Header/DesktopHeader/index.tsx
@@ -1,27 +1,26 @@
 import Logo from "@components/Logo";
-import NavLinks from "./NavLinks";
-import Link from "next/link";
-import { motion } from "framer-motion";
+import { MainNav } from "./MainNav";
+import Search from "@components/ui/search";
+import { UserNav } from "./UserNav";
 
 export default function DesktopHeader() {
   return (
-    <div className="container mx-auto flex h-16 items-center justify-between px-4">
-      <div className="row-center gap-5">
-        <Logo />
-        <NavLinks />
-      </div>
+    <div className="fixed top-0 hidden w-full border-b md:block">
+      <div className="container flex h-16 w-full items-center justify-between gap-4 bg-white">
+        <div className="row-center">
+          {/* 로고 */}
+          <Logo />
+          {/* 네비게이션 */}
+          <MainNav />
+        </div>
 
-      <motion.div
-        whileTap={{ scale: 0.9, transition: { duration: 0.05 } }}
-        whileHover={{ scale: 1.1, transition: { duration: 0.05 } }}
-      >
-        <Link
-          href={"/signin"}
-          className="col-center rounded-full bg-main_color_yellow px-4 py-1 text-white"
-        >
-          <button>로그인</button>
-        </Link>
-      </motion.div>
+        <div className="ml-auto flex items-center space-x-4">
+          {/* 검색 아이콘 */}
+          <Search />
+          {/* 유저 프로필 드롭다운 */}
+          <UserNav />
+        </div>
+      </div>
     </div>
   );
 }
