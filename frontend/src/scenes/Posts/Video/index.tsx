3876da302f87dc295456d9d197b6b5d5a59ee5e7
Kim Yoo <kimyoo04eco@naver.com>
2023-06-04 03:03:21 +0900

    feat: 영상자랑게시판 페이지 생성

---
+++ frontend/src/scenes/Posts/Video/index.tsx
@@ -0,0 +1,18 @@
+import ScrollButton from "@components/ScrollButton";
+import PostList from "@scenes/Posts/PostItem/PostList";
+import CreateButton from "@scenes/Posts/PostItem/CreateButton";
+
+export default function VideoPosts() {
+  return (
+    <div className="w-full">
+      {/* 자유게시판 fetch 결과 출력 */}
+      <PostList />
+
+      {/* 게시글 추가 버튼 */}
+      <CreateButton />
+
+      {/* 최상단 이동 버튼 */}
+      <ScrollButton />
+    </div>
+  );
+}
