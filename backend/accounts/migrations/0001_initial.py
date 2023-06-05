9ed0f589893d1beb239545e582c7411325a79642
wath1457 <wath1457@gmail.com>
2023-06-05 21:43:11 +0900

    style: 열 코딩 컨벤션 준수(pep8)

--- backend/accounts/migrations/0001_initial.py
+++ backend/accounts/migrations/0001_initial.py
@@ -31,7 +31,7 @@ class Migration(migrations.Migration):
                     models.BooleanField(
                         default=False,
                         help_text="Designates that this user has all \
-                            permissions without explicitly assigning them.",
+permissions without explicitly assigning them.",
                         verbose_name="superuser status",
                     ),
                 ),
@@ -52,7 +52,7 @@ class Migration(migrations.Migration):
                     models.BooleanField(
                         default=False,
                         help_text="Designates whether the user can\
-                            log into this admin site.",
+log into this admin site.",
                         verbose_name="staff status",
                     ),
                 ),
@@ -61,8 +61,7 @@ class Migration(migrations.Migration):
                     models.BooleanField(
                         default=True,
                         help_text="Designates whether this user should be \
-                            treated as active. \
-                                Unselect this instead of deleting accounts.",
+treated as active. Unselect this instead of deleting accounts.",
                         verbose_name="active",
                     ),
                 ),
@@ -94,8 +93,7 @@ class Migration(migrations.Migration):
                     models.ManyToManyField(
                         blank=True,
                         help_text="The groups this user belongs to. A user \
-                            will get all permissions \
-                                granted to each of their groups.",
+will get all permissions granted to each of their groups.",
                         related_name="user_set",
                         related_query_name="user",
                         to="auth.group",
--- backend/accounts/migrations/0002_rename_profile_img_user_profile_image_and_more.py
+++ backend/accounts/migrations/0002_rename_profile_img_user_profile_image_and_more.py
@@ -26,7 +26,8 @@ class Migration(migrations.Migration):
             name="groups",
             field=models.ManyToManyField(
                 blank=True,
-                help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+                help_text="The groups this user belongs to. A user will \
+get all permissions granted to each of their groups.",
                 related_name="user_set",
                 related_query_name="user",
                 to="auth.group",
@@ -38,7 +39,8 @@ class Migration(migrations.Migration):
             name="is_active",
             field=models.BooleanField(
                 default=True,
-                help_text="Designates whether this user should be treated as active. Unselect this instead of deleting accounts.",
+                help_text="Designates whether this user should be treated\
+as active. Unselect this instead of deleting accounts.",
                 verbose_name="active",
             ),
         ),
@@ -47,7 +49,8 @@ class Migration(migrations.Migration):
             name="is_staff",
             field=models.BooleanField(
                 default=False,
-                help_text="Designates whether the user can log into this admin site.",
+                help_text="Designates whether the user can log into\
+this admin site.",
                 verbose_name="staff status",
             ),
         ),
@@ -56,7 +59,8 @@ class Migration(migrations.Migration):
             name="is_superuser",
             field=models.BooleanField(
                 default=False,
-                help_text="Designates that this user has all permissions without explicitly assigning them.",
+                help_text="Designates that this user has all permissions\
+without explicitly assigning them.",
                 verbose_name="superuser status",
             ),
         ),
