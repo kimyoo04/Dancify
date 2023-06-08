1dbc74965a27dd8cca263518c22e1444cd3780fd
Kim Yoo <kimyoo04eco@naver.com>
2023-06-08 22:16:44 +0900

    fix: TabBar 컴포넌트에 ScrollButton 컴포넌트 배치

--- frontend/src/layouts/TabBar/index.tsx
+++ frontend/src/layouts/TabBar/index.tsx
@@ -1,25 +1,32 @@
+import ScrollButton from "@components/ScrollButton";
 import { Book, Heart, Home, ShoppingBagIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function TabBar() {
   return (
-    <div className="container fixed bottom-0 grid h-16 w-screen grid-cols-4 justify-between border-t md:hidden">
-      <Link href="/" className="col-center">
-        <Home />
-        <span className="text-xs">Home</span>
-      </Link>
-      <Link href="/posts" className="col-center">
-        <Book />
-        <span className="text-xs">Posts</span>
-      </Link>
-      <Link href="/likes" className="col-center">
-        <Heart />
-        <span className="text-xs">Likes</span>
-      </Link>
-      <Link href="/storage" className="col-center">
-        <ShoppingBagIcon />
-        <span className="text-xs">Storage</span>
-      </Link>
-    </div>
+    <>
+      {/* 최상단 이동 버튼 */}
+      <ScrollButton />
+
+      {/* 탭바 영역 */}
+      <div className="container fixed bottom-0 grid h-16 w-screen grid-cols-4 justify-between border-t bg-background md:hidden">
+        <Link href="/" className="col-center">
+          <Home />
+          <span className="text-xs">Home</span>
+        </Link>
+        <Link href="/posts" className="col-center">
+          <Book />
+          <span className="text-xs">Posts</span>
+        </Link>
+        <Link href="/likes" className="col-center">
+          <Heart />
+          <span className="text-xs">Likes</span>
+        </Link>
+        <Link href="/storage" className="col-center">
+          <ShoppingBagIcon />
+          <span className="text-xs">Storage</span>
+        </Link>
+      </div>
+    </>
   );
 }
