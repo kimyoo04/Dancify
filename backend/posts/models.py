81a577275f883f98cbb8121f673d6473c4c6c626
bfmeef <sumin000619@gmail.com>
2023-06-10 21:37:49 +0900

    feat: 영상자랑, 댄서 게시판 모델 추가

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -5,14 +5,43 @@ import uuid
 from accounts.models import User
 
 
-# Create your models here.
-class FreePost(models.Model):
+class PostBaseModel(models.Model):
+    """
+    게시글마다 공통 컬럼이 있으므로 이를 상속할 베이스 모델을 생성합니다.
+    """
     post_id = models.UUIDField(primary_key=True,
                                default=uuid.uuid4,
                                editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=50)
     content = models.CharField(max_length=1000)
     create_date = models.DateField(auto_now_add=True)
-    post_image = models.URLField(max_length=500, null=True, blank=True)
     views = models.IntegerField(default=0)
+
+    class Meta:
+        abstract = True
+
+
+class VideoPostBaseModel(PostBaseModel):
+    """
+    PostBaseModel을 상속받아 게시글 공통 컬럼을 가지는 모델입니다.
+    영상 게시판은 공통적으로 영상 주소와 썸네일 이미지를 가지므로
+    이를 상속할 베이스 모델을 생성합니다.
+    """
+    video_url = models.URLField(max_length=500, null=False)
+    thumbnail_image = models.URLField(max_length=500, null=False)
+
+    class Meta:
+        abstract = True
+
+
+class FreePost(PostBaseModel):
+    post_image = models.URLField(max_length=500, null=True, blank=True)
+
+
+class VideoBoastPost(VideoPostBaseModel):
+    pass
+
+
+class DancerPost(VideoPostBaseModel):
+    feedback_price = models.IntegerField()
