b1d6a7c8293e65efeeff1db0b5868f19550493aa
Kim Yoo <kimyoo04eco@naver.com>
2023-06-12 01:41:17 +0900

    feat: 인증 안된 클라이언트의 접근 방어

--- frontend/pages/signup/index.tsx
+++ frontend/pages/signup/index.tsx
@@ -1,11 +1,7 @@
 import AuthLayout from "@layouts/AuthLayout";
 import SignUp from "@scenes/SignUp";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Sign Up",
-  description: "Sign Up forms built using the components.",
-};
+import { GetServerSideProps } from "next";
+import { verify } from "jsonwebtoken";
 
 export default function SignUpPage() {
   return (
@@ -14,3 +10,32 @@ export default function SignUpPage() {
     </AuthLayout>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  try {
+    const { req } = ctx;
+    const token = req.cookies["Access-Token"];
+    const secret = process.env.NEXT_PUBLIC_ENV_JWT_SECRET_KEY;
+    if (token && secret) {
+      verify(token, secret);
+    } else {
+      // 정상
+      return {
+        props: {},
+      };
+    }
+
+    // Home 페이지로 리다이렉트
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+    };
+  } catch (error) {
+    // 정상
+    return {
+      props: {},
+    };
+  }
+};
