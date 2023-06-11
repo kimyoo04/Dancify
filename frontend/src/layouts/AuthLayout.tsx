0ecd8e556f40725342e2458caa772da909907087
Kim Yoo <kimyoo04eco@naver.com>
2023-06-12 00:28:12 +0900

    delete: 불필요한 Alert 컴포넌트와 alert state 제거

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -1,15 +1,11 @@
 import Header from "@layouts/Header";
 import TabBar from "@layouts/TabBar";
-import Alert from "@components/Alert";
-import { useAppSelector } from "@toolkit/hook";
 
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const isAlert = useAppSelector((state) => state.alert.isAlert);
-
   return (
     <>
       <div>
@@ -19,9 +15,6 @@ export default function AuthLayout({
       {/* 레이아웃 요소 */}
       <Header />
       <TabBar />
-
-      {/* 알림창 */}
-      {isAlert && <Alert />}
     </>
   );
 }
