3eb1974f6afe3b66490de56441720a9194a71fad
Kim Yoo <kimyoo04eco@naver.com>
2023-06-12 20:27:27 +0900

    fix: 댄서게시판, 자랑게시판, 자유게시판 요청 데이터의 타입 수정

--- frontend/src/type/dancerPosts.ts
+++ frontend/src/type/dancerPosts.ts
@@ -1,71 +1,76 @@
-import { TNickname, TUserId } from "./auth";
-import { TComment, TCommentCount } from "./comments";
+import { TNickname } from "./auth";
+import { TCommentCount, TComment } from "./comments";
 import { TLikesCount } from "./like";
 import {
-  TPostId,
-  TTitle,
   TContent,
-  TPostImage,
+  TPostId,
+  Tthumbnail,
+  TTitle,
+  TVideo,
   TViews,
   TcreateDate,
 } from "./posts";
 
-//! 자유게시판의 1개 게시글 (확정)
-export interface IFreePost {
+//! 댄서게시판의 1개 게시글 (확정)
+export interface IDancerPost {
   postId: TPostId;
   title: TTitle;
   nickname: TNickname;
   content: TContent;
   createDate: TcreateDate;
-  postImage: TPostImage;
+  thumbnail: Tthumbnail;
+  video: TVideo;
   views: TViews;
   commentsCount: TCommentCount;
   likesCount: TLikesCount;
+  feedbackPrice: TFeedbackPrice;
 }
-
-//! 자유게시판의 무한스크롤 데이터 (확정)
-export interface IFreePostsPerPage {
-  data: IFreePost[];
+//! 댄서게시판의 무한스크롤 데이터 (확정)
+export interface IDancerPostsPerPage {
+  data: IDancerPost[];
   totalPages: number;
   currentPage: number;
   totalCount: number;
 }
 
-//! 자유게시글 상세페이지 데이터 (확정)
-export interface IFreePostDetail {
+//! 댄서게시글 상세 페이지 데이터 (확정)
+export interface IDancerPostDetail {
   postId: TPostId;
   title: TTitle;
-  userId: TUserId;
+  userId: string;
   userLike: boolean; // 미완
   nickname: TNickname;
   content: TContent;
   createDate: TcreateDate;
-  postImage: TPostImage;
+  thumbnail: Tthumbnail;
+  video: TVideo;
   views: TViews;
   likesCount: TLikesCount;
+  feedbackPrice: TFeedbackPrice;
   comments: TComment[];
 }
 
-export interface IFreePostDataArr {
-  data: IFreePost[];
+export interface IDancerPostDataArr {
+  data: IDancerPost[];
 }
 
-//! 자유게시글 업로드 폼 (확정)
+// 댄서게시글 업로드 폼
 export interface IPostForm {
   title: TTitle;
   content: TContent;
-  postImage: TPostImage;
+  video: string;
 }
-
-//! 자유게시글 업데이트 폼 (확정)
+//게시글 업데이트 폼
 export interface IUpdatePostForm {
   postId: TPostId; // url에 포함
   title: TTitle;
   content: TContent;
-  postImage: TPostImage;
+  video: string;
 }
 
-//! 자유게시글 삭제 시 전송할 데이터 (확정)
+// 게시글 삭제 시 전송할 데이터
 export interface IDeletePost {
   postId: TPostId; // url에 포함
 }
+
+export type TFeedbackPrice = number;
