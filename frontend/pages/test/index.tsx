b7328ee8ab185bb0293d3b1d6ee345cc59614469
Kim Yoo <kimyoo04eco@naver.com>
2023-06-12 12:06:52 +0900

    feat: movenet의 좌표를 매핑하는 코드 추가

--- frontend/pages/test/index.tsx
+++ frontend/pages/test/index.tsx
@@ -1,11 +1,11 @@
-import AuthLayout from "@layouts/AuthLayout";
+import OnePageLayout from "@layouts/OnePageLayout";
 import Test from "@scenes/Test";
 import React from "react";
 
 export default function SignUpPage() {
   return (
-    <AuthLayout>
+    <OnePageLayout>
       <Test />
-    </AuthLayout>
+    </OnePageLayout>
   );
 }
