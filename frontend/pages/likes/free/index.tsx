fe345cbcb9a9cb6c7cbca785851a7dee95d9c65c
Kim Yoo <kimyoo04eco@naver.com>
2023-06-13 10:48:27 +0900

    feat: likes의 목록 페이지 생성

---
+++ frontend/pages/likes/free/index.tsx
@@ -0,0 +1,10 @@
+import MainLayout from "@layouts/MainLayout";
+import LikeFreePosts from "@scenes/Likes/Free";
+
+export default function LikeFreePostsPage() {
+  return (
+    <MainLayout>
+      <LikeFreePosts />
+    </MainLayout>
+  );
+}
