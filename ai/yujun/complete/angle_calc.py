3561c8deaabbc17a677d784cbaada19f42151dbc
Yujun Won <yujunwon27@naver.com>
2023-06-13 21:29:12 +0900

    fix: angle_calc.py를 movenet 출력에 맞게 수정

--- ai/yujun/complete/angle_calc.py
+++ ai/yujun/complete/angle_calc.py
@@ -1,29 +1,31 @@
 from typing import Any, Dict, List
 import numpy as np
-from typing import List, Dict, Any
 
 
-def get_keypoints(keypoint_list: List[Dict[str, Any]], start_index: int = 5, end_index: int = 16) -> List[List[Any]]:
-    """
-    주어진 keypoint list에서 지정된 범위의 관절 정보를 추출하여 리스트로 반환합니다.
+def get_keypoints(json_data: List[Dict]) -> List[List[Any]]:
+    """주어진 json_data에서 fps를 제외한 모든 keypoint 정보를 추출하여 반환합니다.
 
     Args:
-        keypoint_list (List[Dict[str, Any]]): 관절 정보를 포함하는 샘플 리스트
-        start_index (int): 추출할 관절 정보의 시작 인덱스 (기본값: 5)
-        end_index (int): 추출할 관절 정보의 끝 인덱스 (기본값: 16)
+        json_data (List[Dict]): json 데이터가 포함된 리스트입니다. 리스트의 첫 번째 요소는 fps 정보이며, 이후 요소들은 keypoint 정보가 포함된 딕셔너리입니다.
+            각 keypoint는 다음과 같은 키를 포함하는 딕셔너리여야 합니다:
+            - 'keypoints' (List[Dict]): keypoint의 세부 정보가 포함된 리스트입니다. 각 keypoint는 다음과 같은 키를 포함하는 딕셔너리여야 합니다:
+                - 'x' (float): keypoint의 x 좌표입니다.
+                - 'y' (float): keypoint의 y 좌표입니다.
+                - 'name' (str): keypoint의 이름입니다.
 
     Returns:
-        List[List[Any]]: 추출된 관절 정보가 포함된 리스트 [x: int, y: int, 'name': str]
+        List[List[Any]]: 추출된 관절 정보가 포함된 리스트 [x: float, y: float, 'name': str]
     """
     kps = []
-
-    for idx in range(len(keypoint_list)):
+    for idx in range(1, len(json_data)):
         nth_kp = []
-        for i in range(start_index, end_index + 1):
-            keypoint_info = keypoint_list[idx]['keypoints'][i]
-            x = keypoint_info['x']
-            y = keypoint_info['y']
-            name = keypoint_info['name']
+        keypoint = json_data[idx]
+
+        for i in range(5, 17):
+            keypoint_info = keypoint[0]['keypoints']
+            x = keypoint_info[i]['x']
+            y = keypoint_info[i]['y']
+            name = keypoint_info[i]['name']
             nth_kp.append([x, y, name])
         kps.append(nth_kp)
 
@@ -61,17 +63,16 @@ def calculate_skeleton_angles(keypoint_list: List[List[List[float]]]) -> List[Di
 
     for i, kps in enumerate(keypoint_list):
         nth_skeleton = {
-            # 왼쪽 어깨와 왼쪽 팔꿈치 점을 이은 선이 원점과 이루는 각도
-            "left_upperarm": calculate_angle(kps[0][0], kps[0][1], kps[2][0], kps[2][1]),
-            "right_upperarm": calculate_angle(kps[1][0], kps[1][1], kps[3][0], kps[3][1]),
-            "left_forearm": calculate_angle(kps[2][0], kps[2][1], kps[4][0], kps[4][1]),
-            "right_forearm": calculate_angle(kps[3][0], kps[3][1], kps[5][0], kps[5][1]),
-            "left_waist": calculate_angle(kps[0][0], kps[0][1], kps[6][0], kps[6][1]),
-            "right_waist": calculate_angle(kps[1][0], kps[1][1], kps[7][0], kps[7][1]),
-            "left_thigh": calculate_angle(kps[6][0], kps[6][1], kps[8][0], kps[8][1]),
-            "right_thigh": calculate_angle(kps[7][0], kps[7][1], kps[9][0], kps[9][1]),
-            "left_calf": calculate_angle(kps[8][0], kps[8][1], kps[10][0], kps[10][1]),
-            "right_calf": calculate_angle(kps[9][0], kps[9][1], kps[11][0], kps[11][1]),
+            "left_upperarm": calculate_angle(kps[0][0], kps[0][1], kps[2][0], kps[2][1]) * 180 / np.pi,
+            "right_upperarm": calculate_angle(kps[1][0], kps[1][1], kps[3][0], kps[3][1]) * 180 / np.pi,
+            "left_forearm": calculate_angle(kps[2][0], kps[2][1], kps[4][0], kps[4][1]) * 180 / np.pi,
+            "right_forearm": calculate_angle(kps[3][0], kps[3][1], kps[5][0], kps[5][1]) * 180 / np.pi,
+            "left_waist": calculate_angle(kps[0][0], kps[0][1], kps[6][0], kps[6][1]) * 180 / np.pi,
+            "right_waist": calculate_angle(kps[1][0], kps[1][1], kps[7][0], kps[7][1]) * 180 / np.pi,
+            "left_thigh": calculate_angle(kps[6][0], kps[6][1], kps[8][0], kps[8][1]) * 180 / np.pi,
+            "right_thigh": calculate_angle(kps[7][0], kps[7][1], kps[9][0], kps[9][1]) * 180 / np.pi,
+            "left_calf": calculate_angle(kps[8][0], kps[8][1], kps[10][0], kps[10][1]) * 180 / np.pi,
+            "right_calf": calculate_angle(kps[9][0], kps[9][1], kps[11][0], kps[11][1]) * 180 / np.pi,
             "frame_no": i
         }
         skeleton_angle_list.append(nth_skeleton)
@@ -90,6 +91,7 @@ def calculate_joint_angles(keypoint_list: List[List[List[float]]]) -> List[Dict[
         List[Dict[str, Any]]: 관절 각도 정보를 담고 있는 리스트
     """
     joint_angle_list = []
+    frame_no = 0
 
     for kps in keypoint_list:
         left_upperarm = calculate_angle(
@@ -122,8 +124,9 @@ def calculate_joint_angles(keypoint_list: List[List[List[float]]]) -> List[Dict[
             "right_elbow_joint": abs(right_upperarm - right_forearm) * 180 / np.pi,
             "left_knee_joint": abs(left_thigh - left_calf) * 180 / np.pi,
             "right_knee_joint": abs(right_thigh - right_calf) * 180 / np.pi,
-            "frame_no": keypoint_list.index(kps)
+            "frame_no": frame_no
         }
         joint_angle_list.append(nth_joint)
+        frame_no += 1
 
     return joint_angle_list
--- ai/yujun/complete/note.md
+++ ai/yujun/complete/note.md
@@ -5,11 +5,13 @@
 📦complete
  ┣ 📂notebooks
  ┃ ┣ 📜angle1.ipynb
- ┃ ┗ 📜angle2.ipynb
+ ┃ ┣ 📜angle2.ipynb
+ ┃ ┗ 📜angle3.ipynb
  ┣ 📂point_sample
  ┃ ┣ 📜chansol.json
  ┃ ┣ 📜heewon.json
- ┃ ┗ 📜sample.json
+ ┃ ┣ 📜sample.json
+ ┃ ┗ 📜test_data.json
  ┣ 📜angle_calc.py
  ┣ 📜example.py
  ┗ 📜note.md
@@ -21,9 +23,12 @@
         - 특정 부분만 떼어 내어 향후 진행 방안에 대한 탐구
     - `angle2.ipynb`
         - `angle1.ipynb`에 기반하여, 골격과 관절에 대한 `arctan2()` 각도 추출
+    - `angle3.ipynb`
+        - 희원님께 받은 Movenet 추출 JSON의 포맷에 대응하기 위해 `angle_calc.py` 일부 변경
 - `point_sample`
     - `chansol.json`: 찬솔님께 받은 AI-HUB의 이미지에서 추출한 keypoint
     - `heewon.json`: 희원님께 받은 Movenet에서 추출한 keypoint
     - `sample.json`: `chansol.json`과 `heewon.json`을 하나의 리스트에 담은 파일
+    - `test_data.json`: 희원님께 받은 Movenet 카리나 추출 데이터(30fps, 20sec → 602frame) 하나의 리스트에 담은 파일
 - `angle_calc.py`: 각도 추출이 구현된 함수
 - `example.py`: `angle_calc.py`를 실제 사용한 예시
