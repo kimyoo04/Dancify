3792065629d3105e576fcfc0f4c86d9f3707c71a
Leechansol <chsol3391@gmail.com>
2023-06-14 16:37:37 +0900

    feat: vito함수, roberta모델

--- ai/chansol/research/vitotest/vito_auth.py
+++ ai/chansol/research/vitotest/vito_auth.py
@@ -3,8 +3,8 @@ import requests
 
 resp = requests.post(
     'https://openapi.vito.ai/v1/authenticate',
-    data={'client_id': '',
-          'client_secret': ''}
+    data={'client_id': 'D_QV6Y2866NzPkULcJTh',
+          'client_secret': 'rJ7hm9FpPS5yhQ2oUpQre4Wp9KYfQP89S4BZtr4P'}
 )
 resp.raise_for_status()
 print(resp.json())
--- ai/chansol/research/vitotest/vito_post.py
+++ ai/chansol/research/vitotest/vito_post.py
@@ -7,12 +7,12 @@ config = {
     },
     "use_multi_channel": False
 }
-j_key = 'jwt_token'
+j_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODY3Mzg3OTQsImlhdCI6MTY4NjcxNzE5NCwianRpIjoiUS1OcmlPQXZQeEhZWTNCRnNxS0wiLCJwbGFuIjoiYmFzaWMiLCJzY29wZSI6InNwZWVjaCIsInN1YiI6IkRfUVY2WTI4NjZOelBrVUxjSlRoIn0.6N2gJOQyyzJImO98DK-Lh_F777ir0T9Zyy2N2ychJ3A'
 resp = requests.post(
     'https://openapi.vito.ai/v1/transcribe',
     headers={'Authorization': 'bearer '+j_key},
     data={'config': json.dumps(config)},
-    files={'file': open('daum.m4a', 'rb')}
+    files={'file': open('sample4.m4a', 'rb')}
 )
 resp.raise_for_status()
 print(resp.json()['id'])  # transcribe_id
