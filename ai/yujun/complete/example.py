508df595c958142e16d5793751f3fe7ad21d139a
Yujun Won <yujunwon27@naver.com>
2023-06-14 15:21:41 +0900

    fix: arctan2에서 벡터의 내적으로 관절 각도 구하는 원리 교체

--- ai/yujun/complete/example.py
+++ ai/yujun/complete/example.py
@@ -1,12 +1,10 @@
 import angle_calc
-import numpy as np
-import matplotlib.pyplot as plt
 import json
 import sys
 
 sys.path.append('/Users/yujunwon/Project/dancify/ai/yujun/complete/')
 
-with open("ai/yujun/complete/point_sample/sample.json", 'r') as f:
+with open("ai/yujun/complete/point_sample/test_data.json", 'r') as f:
     json_data = json.load(f)
 
 # 1. JSON에서 사용할 keypoint를 추출
@@ -16,14 +14,7 @@ print(keypoints)
 print(f"keypoints 리스트의 길이: {len(keypoints)}")
 print('-' * 100)
 
-# 2. 추출한 keypoint를 이용하여 골격의 arctan2를 계산
-skeletons = angle_calc.calculate_skeleton_angles(keypoints)
-
-print(skeletons)
-print(f"skeletons 리스트의 길이: {len(skeletons)}")
-print('-' * 100)
-
-# 3. 추출한 keypoint를 이용하여 관절의 arctan2를 계산
+# 2. 추출한 keypoint를 이용하여 관절의 arctan2를 계산
 joints = angle_calc.calculate_joint_angles(keypoints)
 
 print(joints)
