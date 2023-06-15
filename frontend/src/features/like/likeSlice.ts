322e8169827813a3be399f8e8c9632fe31905949
Kim Yoo <kimyoo04eco@naver.com>
2023-06-15 20:17:46 +0900

    delete: 불필요한 likeSlice reducer 제거

--- frontend/src/features/like/likeSlice.ts
+++ frontend/src/features/like/likeSlice.ts
@@ -1,32 +1,16 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
-import { ILikeState, ILikeAction } from "@type/like";
+import { createSlice } from "@reduxjs/toolkit";
+import { ILikeState } from "@type/like";
 
 const initialState: ILikeState = {
   userLike: false,
-  postId: "",
-  postCategory: "",
 };
 
 export const likeSlice = createSlice({
   name: "like",
   initialState,
   reducers: {
-    // 최근 검색어 1개 제거
-    getDetailPost: (state, actions: PayloadAction<ILikeAction>) => {
-      state.userLike = actions.payload.userLike;
-      state.postId = actions.payload.postId;
-      state.postCategory = actions.payload.postCategory;
-    },
-
-    // TODO 비동기 처리 고려하기
     toggleLike: (state) => {
       state.userLike = !state.userLike;
-    },
-
-    resetDetailPost: (state) => {
-      state.userLike = false;
-      state.postId = "";
-      state.postCategory = "";
     },
   },
 });
