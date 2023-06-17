9ead89519a6d3d2263197b2b689a30690c73b952
Kim Yoo <kimyoo04eco@naver.com>
2023-06-17 10:41:47 +0900

    delete: SearchKeywordsHeader, SearchRankHeader 삭제

--- frontend/src/layouts/Header/Search/SearchPopUp/SearchRank/SearchRankHeader.tsx
+++
@@ -1,7 +0,0 @@
-export default function SearchRankHeader() {
-  return (
-    <div className="flex items-center justify-between rounded-t-md bg-muted px-4 py-2">
-      <h3 className="text-md font-medium">인기 검색어</h3>
-    </div>
-  );
-}
