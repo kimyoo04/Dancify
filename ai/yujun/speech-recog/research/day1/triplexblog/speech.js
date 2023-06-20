b87d53e7b6aea6d80e8ac5a54d5989f87d797c4c
Yujun Won <yujunwon27@naver.com>
2023-06-20 11:34:04 +0900

    feat: 음성 인식 결과를 텍스트로 저장하는 방법 구현

--- ai/yujun/speech-recog/research/day1/triplexblog/speech.js
+++ ai/yujun/speech-recog/research/day1/triplexblog/speech.js
@@ -42,8 +42,16 @@ var speechStore = {
             selector('.dictate').classList.remove('on');
             recognition_1.stop();
             speechStore.isRecognizing = true;
-            setTimeout(downloadCapturedText, 1000); // 1초 지연 후 자동 다운로드
+            // setTimeout(downloadCapturedText, 1000); // 1초 지연 후 자동 다운로드
         };
+        selector('.dictate').addEventListener('click', function () {
+            if (speechStore.isRecognizing) {
+                active_1();
+            }
+            else {
+                unactive_1();
+            }
+        });
         // 캡처한 텍스트를 다운로드하는 함수
         function downloadCapturedText() {
             var element = document.createElement('a');
@@ -53,35 +61,8 @@ var speechStore = {
             document.body.appendChild(element);
             element.click();
         }
-        // // 음성인식 비활성화 2: JSON 다운로드
-        // const unactive = () => {
-        //     (selector('.dictate') as HTMLElement).classList.remove('on')
-        //     recognition.stop();
-        //     speechStore.isRecognizing = true;
-        //     // 로그 다운로드
-        //     fetch('http://127.0.0.1:3300/download_logs', {
-        //         method: 'POST',
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //         },
-        //         body: JSON.stringify({ log: speechStore.texts }),
-        //     })
-        //     .then(response => response.blob())
-        //     .then(blob => {
-        //         const url = window.URL.createObjectURL(blob);
-        //         const a = document.createElement('a');
-        //         a.href = url;
-        //         a.download = 'logs.txt';
-        //         a.click();
-        //     });
-        // };
-        selector('.dictate').addEventListener('click', function () {
-            if (speechStore.isRecognizing) {
-                active_1();
-            }
-            else {
-                unactive_1();
-            }
+        selector('.save').addEventListener("click", function () {
+            downloadCapturedText();
         });
     }
 })();
