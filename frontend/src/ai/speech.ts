8445a64fe478c98d2730fb64dcdf4ebc54af7d4f
Kim <58503130+kimyoo04@users.noreply.github.com>
2023-06-21 11:08:41 +0900

    fix: speech.ts 타입 에러 임시 해결

--- frontend/src/ai/speech.ts
+++ frontend/src/ai/speech.ts
@@ -1,89 +1,90 @@
-interface Window {
-    SpeechRecognition: any;
-    webkitSpeechRecognition: any;
-}
-
 const selector = (el: string): Element | null => document.querySelector(el);
 
 const speechStore = {
-    texts: '',                 // 인식된 텍스트 저장
-    isRecognizing: true        // 현재 음성 인식 중인지를 나타내는 프로퍼티
+  texts: "", // 인식된 텍스트 저장
+  isRecognizing: true, // 현재 음성 인식 중인지를 나타내는 프로퍼티
 };
 
 // 익명 함수를 정의하고 바로 실행 -> 코드 모듈화하여 전역 스코프 오염을 방지
-(() => {
-    /* Speech API start */
-    let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-    // 만약 지원하지 않는 브라우저라면 알림 출력
-    if (!("webkitSpeechRecognition" in window)) {
-        alert("지원하지 않는 브라우저 입니다!")
-    } else {
-        const recognition = new SpeechRecognition();
-        recognition.interimResults = false;     // true: 연속적 음절 인식 / false: 연속적 음절 인식 x
-        recognition.lang = 'ko-KR';             // 값이 없으면 HTML의 <html lang="en">을 참고합니다. ko-KR, en-US
-        recognition.continuous = false;         // true: 연속 결과 반환 / false: 단일 결과 반환
-        recognition.maxAlternatives = 20000;    // 숫자가 작을수록 발음대로 적고, 클수록 문장의 적합도에 따라 알맞은 단어로 대체
+export function speechToText(speakButtonRef: any) {
+  // 만약 지원하지 않는 브라우저라면 알림 출력
+  if (!("webkitSpeechRecognition" in window)) {
+    alert("지원하지 않는 브라우저 입니다!");
+  } else {
+    const recognition = new webkitSpeechRecognition();
+    recognition.interimResults = false; // true: 연속적 음절 인식 / false: 연속적 음절 인식 x
+    recognition.lang = "ko-KR"; // 값이 없으면 HTML의 <html lang="en">을 참고합니다. ko-KR, en-US
+    recognition.continuous = false; // true: 연속 결과 반환 / false: 단일 결과 반환
+    recognition.maxAlternatives = 20000; // 숫자가 작을수록 발음대로 적고, 클수록 문장의 적합도에 따라 알맞은 단어로 대체
 
-        // 음성 인식이 끝났을 때 수행되는 동작
-        recognition.onspeechend = function () {
-            recognition.stop();
-            (selector('.dictate') as HTMLElement).classList.remove("on");   // 버튼 동작(마이크 on/off 표시)
-            speechStore.isRecognizing = true;
-        };
+    // 음성 인식이 끝났을 때 수행되는 동작
+    recognition.onspeechend = function () {
+      recognition.stop();
+      speakButtonRef.classList.remove("on"); // 버튼 동작(마이크 on/off 표시)
+      speechStore.isRecognizing = true;
+    };
 
-        // 음성 인식 서비스 결과 반환
-        recognition.onresult = function (e: { results: SpeechRecognitionResultList; }) {
-            speechStore.texts = Array.from((e.results as SpeechRecognitionResultList))
-                .map(results => results[0].transcript).join("");
+    // 음성 인식 서비스 결과 반환
+    recognition.onresult = function (e: {
+      results: SpeechRecognitionResultList;
+    }) {
+      speechStore.texts = Array.from(e.results as SpeechRecognitionResultList)
+        .map((results) => results[0].transcript)
+        .join("");
 
-            console.log(speechStore.texts);
-            (selector('input') as HTMLInputElement).value = speechStore.texts;
-        };
-        /* Speech API END */
+      console.log(speechStore.texts);
+      (selector("input") as HTMLInputElement).value = speechStore.texts;
+    };
+    /* Speech API END */
 
-        // 음성인식 활성화
-        const active = () => {
-            (selector('.dictate') as HTMLElement).classList.add('on')
-            recognition.start();
-            speechStore.isRecognizing = false;
-        };
+    // 음성인식 활성화
+    const active = () => {
+      speakButtonRef.classList.add("on");
+      recognition.start();
+      speechStore.isRecognizing = false;
+    };
 
-        // 음성인식 비활성화
-        const unactive = () => {
-            (selector('.dictate') as HTMLElement).classList.remove('on')
-            recognition.stop();
-            speechStore.isRecognizing = true;
-        };
+    // 음성인식 비활성화
+    const unactive = () => {
+      speakButtonRef.classList.remove("on");
+      recognition.stop();
+      speechStore.isRecognizing = true;
+    };
 
-        (selector('.dictate') as HTMLElement).addEventListener('click', () => {
-            if (speechStore.isRecognizing) {
-                active();
-            } else {
-                unactive();
-            }
-        });
+    speakButtonRef.addEventListener("click", () => {
+      if (speechStore.isRecognizing) {
+        active();
+      } else {
+        unactive();
+      }
+    });
+  }
+}
 
-        // 텍스트 분류 함수
-        function wordDict(){
-            speechStore.texts = speechStore.texts.replace(' ', '');
-            const WD: Record<string, string[]> = {
-              "이전": ["이전", "이전으로", "이정", "이전구간으로"],
-              "다음": ["다음", "다음으로", "다음순서로", "다음구간으로", "당", "담"],
-              "한번더": ["한번더", "함더", "한번더해", "함덕", "한번더할게"],
-              "종료": ["종료", "종료하다", "종료해", "종료해라", "종료할게", "종료할게요", "종료합니다"]
-            };
+// 텍스트 분류 함수
+export function wordDict() {
+  speechStore.texts = speechStore.texts.replace(" ", "");
+  const WD: Record<string, string[]> = {
+    이전: ["이전", "이전으로", "이정", "이전구간으로"],
+    다음: ["다음", "다음으로", "다음순서로", "다음구간으로", "당", "담"],
+    한번더: ["한번더", "함더", "한번더해", "함덕", "한번더할게"],
+    종료: [
+      "종료",
+      "종료하다",
+      "종료해",
+      "종료해라",
+      "종료할게",
+      "종료할게요",
+      "종료합니다",
+    ],
+  };
 
-            for (const key of Object.keys(WD)) {
-                if (WD[key].includes(speechStore.texts)) {
-                    console.log(key)
-                    return key;
-                }
-            }
-            console.log("기타")
-            return "기타";
-        }
-
+  for (const key of Object.keys(WD)) {
+    if (WD[key].includes(speechStore.texts)) {
+      console.log(key);
+      return key;
     }
-})();
-
-
+  }
+  console.log("기타");
+  return "기타";
+}
