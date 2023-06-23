9f60a5a68bb96cf058b0ff0ce068fcd14decf2d2
Kim <58503130+kimyoo04@users.noreply.github.com>
2023-06-24 02:10:06 +0900

    style: build 에러 해결

--- frontend/pages/practice/[postId].tsx
+++ frontend/pages/practice/[postId].tsx
@@ -1,6 +1,8 @@
 import PracticeLayout from "@layouts/PracticeLayout";
 import Practice from "@scenes/Practice";
 import { TPostId } from "@type/posts";
+import { verify } from "jsonwebtoken";
+import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 
 export default function CreatePostPage() {
@@ -13,3 +15,35 @@ export default function CreatePostPage() {
     </PracticeLayout>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  try {
+    const { req } = ctx;
+    const token = req.cookies["Access-Token"];
+    const secret = process.env.NEXT_PUBLIC_ENV_JWT_SECRET_KEY;
+    if (token && secret) {
+      verify(token, secret);
+    } else {
+      // Sign In 페이지로 리다이렉트
+      return {
+        redirect: {
+          permanent: false,
+          destination: "/signin",
+        },
+      };
+    }
+
+    // 정상
+    return {
+      props: {},
+    };
+  } catch (error) {
+    return {
+      // Sign In 페이지로 리다이렉트
+      redirect: {
+        permanent: false,
+        destination: "/signin",
+      },
+    };
+  }
+};
