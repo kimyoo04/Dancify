c828bf4f33fe7293bcbc6378c01555fc9bea6043
bfmeef <sumin000619@gmail.com>
2023-06-24 20:38:43 +0900

    style: pep8 컨벤션 준수

--- backend/search_history/management/commands/seed_search.py
+++ backend/search_history/management/commands/seed_search.py
@@ -1,25 +1,15 @@
-from random import randint, random, choice
+from random import randint, choice
 from typing import Any
-from django.core.management.base import BaseCommand, CommandParser
+from django.core.management.base import BaseCommand
 from django_seed import Seed
 
-from accounts.models import User
 from ...models import SearchHistory
 
 
 class Command(BaseCommand):
     help = '이 커맨드를 통해 랜덤한 자유 게시판, 영상 자랑 게시판, 댄서 영상 게시판 데이터 생성.'
 
-    def add_arguments(self, parser: CommandParser) -> None:
-        parser.add_argument(
-            '--number',
-            default=10,
-            type=int,
-            help='데이터를 얼마나 생성할 것인지 결정'
-        )
-
     def handle(self, *args: Any, **options: Any) -> str | None:
-        number = options.get('number')
         seeder = Seed.seeder()
 
         category = ['FREE', 'VIDEO', 'DANCER']
