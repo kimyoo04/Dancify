0bd11eed84279cc611d3b085c17e1ccb727fce78
bfmeef <sumin000619@gmail.com>
2023-06-25 23:54:05 +0900

    fix: 댄서게시판 더미데이터 변경

--- backend/posts/management/commands/seed_posts.py
+++ backend/posts/management/commands/seed_posts.py
@@ -27,29 +27,37 @@ class Command(BaseCommand):
                            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/post-image/randomURL2.png',
                            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/post-image/randomURL3.jpg']
         thumbnail_urls = [
-            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/dancable1/6015493aa1cc4ff78eaaabe449cc1775.JPG',
-            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/dancable1/e1a27bdfc7f445f0a15b457de5d9f427.JPG',
-            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/dancable2/fc56b71542754b3bb134b065b186b1e9.JPG',
-            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/user2/d7ac59c77d7a46d2ac34a71bb7fc72ab.JPG',
-            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/dancer1/9ffcfde840fd41268b8eed8a7db133c0.JPG',
-            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/dancer2/de2b6b00b3a34bd39566d8d12fa07f18.JPG',
-            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/dancer3/98e64117169e4031821875021b64895d.JPG',
-            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/user1/cb03295aa6eb470bb24423d860501860.JPG',
-            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/user2/6e1a433090f545c78647ad8f5e759cde.JPG',
-            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/dancer1/5425e72fc9b9498ea221b6d1854e0568.JPG',
+            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/danceable1/dasd22141sdd12e21a-thumbnail.0000000.jpg',
+            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/danceable2/daslidj2189dhjq8wdjh-thumbnail.0000000.jpg',
+            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/user2/fdad21421098qwdd-thumbnail.0000000.jpg',
+            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/dancer1/di391d890j31d8j231d8213jd-thumbnail.0000000.jpg',
+            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/dancer2/32e1209ujqwi0dj01289jd12-thumbnail.0000000.jpg',
+            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/dancer3/fd32fhj890fjwefiwefjwe-thumbnail.0000000.jpg',
+            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/user1/398ry238fh23ofj9280fjef-thumbnail.0000000.jpg',
+            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/user2/32098du239dj2l3kd-thumbnail.0000000.jpg',
+            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/thumbnail/dancer1/d2390dj23dj2309f2f32-thumbnail.0000000.jpg',
         ]
 
         video_urls = [
-            'http://dyago72jbsqcn.cloudfront.net/vod/danceable/dancable1/6015493aa1cc4ff78eaaabe449cc1775.m3u8',
-            'http://dyago72jbsqcn.cloudfront.net/vod/danceable/dancable1/e1a27bdfc7f445f0a15b457de5d9f427.m3u8',
-            'http://dyago72jbsqcn.cloudfront.net/vod/danceable/dancable2/fc56b71542754b3bb134b065b186b1e9.m3u8',
-            'http://dyago72jbsqcn.cloudfront.net/vod/danceable/user2/d7ac59c77d7a46d2ac34a71bb7fc72ab.m3u8',
-            'http://dyago72jbsqcn.cloudfront.net/vod/dancer/dancer1/9ffcfde840fd41268b8eed8a7db133c0.m3u8',
-            'http://dyago72jbsqcn.cloudfront.net/vod/dancer/dancer2/de2b6b00b3a34bd39566d8d12fa07f18.m3u8',
-            'http://dyago72jbsqcn.cloudfront.net/vod/dancer/dancer3/98e64117169e4031821875021b64895d.m3u8',
-            'http://dyago72jbsqcn.cloudfront.net/vod/boast/user1/cb03295aa6eb470bb24423d860501860.m3u8',
-            'http://dyago72jbsqcn.cloudfront.net/vod/boast/user2/6e1a433090f545c78647ad8f5e759cde.m3u8',
-            'http://dyago72jbsqcn.cloudfront.net/vod/feedback/dancer1/5425e72fc9b9498ea221b6d1854e0568.m3u8',
+            'http://dyago72jbsqcn.cloudfront.net/vod/danceable/danceable1/dasd22141sdd12e21a.m3u8',
+            'http://dyago72jbsqcn.cloudfront.net/vod/danceable/danceable2/daslidj2189dhjq8wdjh.m3u8',
+            'http://dyago72jbsqcn.cloudfront.net/vod/danceable/user2/fdad21421098qwdd.m3u8',
+            'http://dyago72jbsqcn.cloudfront.net/vod/dancer/dancer1/d2390dj23dj2309f2f32.m3u8',
+            'http://dyago72jbsqcn.cloudfront.net/vod/dancer/dancer2/32e1209ujqwi0dj01289jd12.m3u8',
+            'http://dyago72jbsqcn.cloudfront.net/vod/dancer/dancer3/fd32fhj890fjwefiwefjwe.m3u8',
+            'http://dyago72jbsqcn.cloudfront.net/vod/boast/user1/398ry238fh23ofj9280fjef.m3u8',
+            'http://dyago72jbsqcn.cloudfront.net/vod/boast/user2/32098du239dj2l3kd.m3u8',
+            'http://dyago72jbsqcn.cloudfront.net/vod/feedback/dancer1/d2390dj23dj2309f2f32.m3u8',
+        ]
+
+        keypoints_urls = [
+            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/key-points/dancable1/6015493aa1cc4ff78eaaabe449cc1775.json',
+            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/key-points/dancable1/e1a27bdfc7f445f0a15b457de5d9f427.json',
+            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/key-points/dancable2/fc56b71542754b3bb134b065b186b1e9.json',
+            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/key-points/dancer1/9ffcfde840fd41268b8eed8a7db133c0.json',
+            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/key-points/dancer2/de2b6b00b3a34bd39566d8d12fa07f18.json',
+            'https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/key-points/dancer3/98e64117169e4031821875021b64895d.json',
+            ''
         ]
 
         group_names = ['르세라핌', '유키스', '아이브', '(여자)아이들',
@@ -73,27 +81,31 @@ class Command(BaseCommand):
                           })
 
         # 영상 자랑 게시판 더미데이터 생성
-        seeder.add_entity(VideoPost, number,
-                          {
-                              "user": lambda x: choice(users),
-                              "title": lambda x: seeder.faker.sentence(nb_words=4, variable_nb_words=True, ext_word_list=None) + choice(group_names),
-                              "content": lambda x: seeder.faker.sentence(nb_words=10, variable_nb_words=True, ext_word_list=None),
-                              "video": lambda x: choice(video_urls),
-                              "thumbnail": lambda x: choice(thumbnail_urls),
-                              "views": lambda x: randint(0, 999)
-                          })
+        for i in range(18):
+            seeder.add_entity(VideoPost, 1,
+                            {
+                                "user": lambda x: choice(users),
+                                "title": lambda x: seeder.faker.sentence(nb_words=4, variable_nb_words=True, ext_word_list=None) + choice(group_names),
+                                "content": lambda x: seeder.faker.sentence(nb_words=10, variable_nb_words=True, ext_word_list=None),
+                                "video": video_urls[i % 9],
+                                "thumbnail": thumbnail_urls[i % 9],
+                                "views": lambda x: randint(0, 999)
+                            })
 
         # 댄서 게시판 더미데이터 생성
-        seeder.add_entity(DancerPost, number,
-                          {
-                              "user": lambda x: choice(users),
-                              "title": lambda x: seeder.faker.sentence(nb_words=4, variable_nb_words=True, ext_word_list=None) + choice(group_names),
-                              "content": lambda x: seeder.faker.sentence(nb_words=10, variable_nb_words=True, ext_word_list=None),
-                              "video": lambda x: choice(video_urls),
-                              "thumbnail": lambda x: choice(thumbnail_urls),
-                              "genre": lambda x: choice(['basic', 'kpop']),
-                              "feedback_price": lambda x: randint(10, 99) * 1000,
-                              "views": lambda x: randint(0, 999)
-                          })
+        for i in range(2):
+            for j in range(6):
+                seeder.add_entity(DancerPost, 1,
+                                {
+                                    "user": lambda x: choice(users),
+                                    "title": lambda x: seeder.faker.sentence(nb_words=4, variable_nb_words=True, ext_word_list=None) + choice(group_names),
+                                    "content": lambda x: seeder.faker.sentence(nb_words=10, variable_nb_words=True, ext_word_list=None),
+                                    "video": video_urls[j],
+                                    "thumbnail": thumbnail_urls[j],
+                                    "keypoints": keypoints_urls[j],
+                                    "genre": lambda x: choice(['basic', 'kpop']),
+                                    "feedback_price": lambda x: randint(10, 99) * 1000,
+                                    "views": lambda x: randint(0, 999)
+                                })
 
         seeder.execute()
