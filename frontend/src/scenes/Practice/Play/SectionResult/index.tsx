33927cf01816445beb0079b0c3913d9e74de6ae7
Kim <58503130+kimyoo04@users.noreply.github.com>
2023-06-25 16:03:48 +0900

    refactor: playIndex를 practiceSlice로 생성 및 SectionPlay 컴포넌트로 영상 스트리밍과 웹캠 송출 역할을 분리

--- frontend/src/scenes/Practice/Play/SectionResult/index.tsx
+++ frontend/src/scenes/Practice/Play/SectionResult/index.tsx
@@ -1,14 +1,18 @@
-import StepBar from "./stepBar";
+import { useAppSelector } from "@toolkit/hook";
+import ProgressBar from "./ProgressBar";
 import ScoreBoard from "./ScoreBoard";
 import SpeechCommand from "./SpeechCommand";
 import { IPractice } from "@type/practice";
+import convertToOrdinal from "@util/convertToOrdinal";
 
 export default function SectionResult({ data }: { data: IPractice }) {
+  const { playIndex } = useAppSelector((state) => state.practice);
+
   return (
     <div className="space-y-20">
-      <h1>{" "} 구간 연습 결과</h1>
+      <h1>{convertToOrdinal(playIndex)} 구간 연습 결과</h1>
 
-      <StepBar />
+      <ProgressBar />
 
       <div className="row-center gap-10">
         <ScoreBoard />
