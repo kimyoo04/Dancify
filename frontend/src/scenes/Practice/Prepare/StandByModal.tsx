3220e92674deb0a4ef5bf54d54b61d8efd305688
Kim <58503130+kimyoo04@users.noreply.github.com>
2023-06-25 16:04:34 +0900

    style: prettier 포멧팅

--- frontend/src/scenes/Practice/Prepare/StandByModal.tsx
+++ frontend/src/scenes/Practice/Prepare/StandByModal.tsx
@@ -1,4 +1,8 @@
-export default function StandByModal({ children }: { children?: React.ReactNode }) {
+export default function StandByModal({
+  children,
+}: {
+  children?: React.ReactNode;
+}) {
   return (
     <div className="col-center h-full w-full px-10 sm:px-20">
       <div className="h-[80%] w-full rounded-md bg-background p-6 shadow-md sm:w-[440px]">
