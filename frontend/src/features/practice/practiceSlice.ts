051a5f4d8f9a9c351e9587a443c1a1ce66e669f9
Kim <58503130+kimyoo04@users.noreply.github.com>
2023-06-27 05:06:03 +0900

    fix: practiceSlice의 state 추가 (isRealMode, isFullBody, isFinished, isPlaying)

--- frontend/src/features/practice/practiceSlice.ts
+++ frontend/src/features/practice/practiceSlice.ts
@@ -4,8 +4,12 @@ import { IPracticeState, IUpdateSectionPractice } from "@type/practice";
 const initialState: IPracticeState = {
   step: 1, // 연습의 단계 인덱스
   playIndex: 0, // 영상의 단계 인덱스
+  isRealMode: false, // 실전 모드 유무
   isSkeleton: false, // 스켈레톤 매핑 유무
-  isFinished: false, // 안무 연습 완료 유무
+  isFullBody: false, // 전신 유무
+  isFinished: false, // SectionResult 컴포넌트 랜더링 유무
+  isPlaying: false, // react-player 재생 유무
+
   selectedSections: [],
   sectionPracticeArr: [],
 };
@@ -20,14 +24,35 @@ export const practiceSlice = createSlice({
     },
 
     // 구간 연습 종료
-    finishSectionPractice: (state) => {
+    finishSectionPlay: (state) => {
       state.isFinished = true;
+      state.isPlaying = false;
+    },
+
+    // SectionPlay 컴포넌트 랜더링
+    showSectionPlay: (state) => {
+      state.isFinished = false;
+    },
+
+    // 영상 재생
+    playVideo: (state) => {
+      state.isPlaying = true;
+    },
+
+    // 전신 유무 확인
+    checkFullBody: (state) => {
+      state.isFullBody = true;
     },
 
     // 영상 단계 인덱스 증가
     moveNextSection: (state) => {
       state.playIndex += 1;
       state.isFinished = false;
+    },
+
+    // 실전 모드 토글
+    toggleReal: (state) => {
+      state.isRealMode = !state.isRealMode;
     },
 
     // 스켈레톤 유무 토글
@@ -37,21 +62,24 @@ export const practiceSlice = createSlice({
 
     // section 복수 선택 토글
     toggleSelectedSections: (state, action: PayloadAction<number>) => {
-      const sectionId = action.payload;
+      const sectionIndex = action.payload;
       // 선택된 section이 있는지 확인
-      const sectionIndex = state.selectedSections.findIndex(
-        (section) => section === sectionId
+      const isSection = state.selectedSections.findIndex(
+        (section) => section === sectionIndex
       );
       // 선택된 section이 없으면 추가, 있으면 제거
-      if (sectionIndex === -1) {
-        state.selectedSections.push(sectionId);
+      if (isSection === -1) {
+        state.selectedSections.push(sectionIndex);
       } else {
         state.selectedSections.splice(sectionIndex, 1);
       }
     },
 
     // section의 대한 최초, 최고 점수 입력
-    updateSectionPractice: (state, action: PayloadAction<IUpdateSectionPractice>) => {
+    updateSectionPractice: (
+      state,
+      action: PayloadAction<IUpdateSectionPractice>
+    ) => {
       const { sectionId, score, poseMessages } = action.payload;
       const sectionIndex = state.sectionPracticeArr.findIndex(
         (section) => section.sectionId === sectionId
@@ -89,6 +117,8 @@ export const practiceSlice = createSlice({
       state.playIndex = 0;
       state.isSkeleton = false;
       state.isFinished = false;
+      state.isFullBody= false,
+      state.isPlaying= false,
       state.selectedSections = [];
       state.sectionPracticeArr = [];
     },
