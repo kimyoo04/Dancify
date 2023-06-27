31b24cc8a65bcabf3f6e063bf2e1049e13351c0b
baki <55778040+heewon00@users.noreply.github.com>
2023-06-27 11:50:18 +0900

    feat: pages의 chart test페이지

---
+++ frontend/pages/chart/index.tsx
@@ -0,0 +1,20 @@
+import MainLayout from "@layouts/MainLayout";
+import ScoreBoard from "@scenes/Practice/Result/ScoreBoard";
+import StackedBar from "@scenes/Practice/Result/StackedBarChart";
+import React from "react";
+
+export default function SignUpPage() {
+  const scoreToMessage = {
+    Miss: 6,
+    Good: 45,
+    Great: 23,
+    Excellent: 4,
+  };
+
+  return (
+    <MainLayout>
+      {/* <ScoreBoard scoreToMessage={scoreToMessage} /> */}
+      <StackedBar />
+    </MainLayout>
+  );
+}
