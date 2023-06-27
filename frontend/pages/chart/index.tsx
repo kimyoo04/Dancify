b84b058c64caa2cab724f1173547af0831a279a9
Kim <58503130+kimyoo04@users.noreply.github.com>
2023-06-28 02:01:09 +0900

    fix: 차트 시각화 확인용으로 더미데이터 주입 및 chart 페이지에서 출력

--- frontend/pages/chart/index.tsx
+++ frontend/pages/chart/index.tsx
@@ -1,76 +1,13 @@
 import MainLayout from "@layouts/MainLayout";
-import ScoreBoard from "@scenes/Practice/Result/ScoreBoard";
-import StackedBar from "@scenes/Practice/Result/StackedBarChart";
+import DonutChart from "@scenes/Test/DonutChart";
+import StackedBarChart from "@scenes/Test/StackedBarChart";
 import React from "react";
-import { IPracticeState, IPoseMessages } from "@type/practice";
 
-export default function SignUpPage() {
-  const dummyData1 = {
-    Miss: 6,
-    Good: 45,
-    Great: 23,
-    Excellent: 4,
-  };
-
-  const dummyData2 = {
-    step: "Step 1",
-    playIndex: 1,
-    sectionPracticeArr: [
-      {
-        sectionId: "Section 1",
-        firstScore: 80,
-        bestScore: 90,
-        playCounts: 5,
-        poseMessages: {
-          Miss: 2,
-          Good: 1,
-          Great: 2,
-          Excellent: 5,
-        },
-      },
-      {
-        sectionId: "Section 3",
-        firstScore: 70,
-        bestScore: 85,
-        playCounts: 3,
-        poseMessages: {
-          Miss: 1,
-          Good: 1,
-          Great: 1,
-          Excellent: 3,
-        },
-      },
-      {
-        sectionId: "Section 4",
-        firstScore: 75,
-        bestScore: 95,
-        playCounts: 7,
-        poseMessages: {
-          Miss: 3,
-          Good: 2,
-          Great: 1,
-          Excellent: 1,
-        },
-      },
-      {
-        sectionId: "Section 2",
-        firstScore: 60,
-        bestScore: 80,
-        playCounts: 4,
-        poseMessages: {
-          Miss: 2,
-          Good: 1,
-          Great: 1,
-          Excellent: 0,
-        },
-      },
-    ],
-  };
-
+export default function ChartPage() {
   return (
     <MainLayout>
-      <ScoreBoard scoreToMessage={dummyData1 as IPoseMessages} />
-      <StackedBar Practice={dummyData2 as IPracticeState} />
+      <DonutChart />
+      <StackedBarChart />
     </MainLayout>
   );
 }
---
+++ frontend/src/scenes/Test/StackedBarChart.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { dummy } from "./dummy";
+// import { useAppSelector } from "@toolkit/hook";
+
+export default function StackedBarChart() {
+  // const sectionPracticeArr = useAppSelector(
+  //   (state) => state.practice.sectionPracticeArr
+  // );
+
+  const sectionPracticeArr = dummy.sectionPracticeArr;
+
+  Chart.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const options = {
+    indexAxis: "y" as const,
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: "bottom" as const,
+      },
+    },
+    scales: {
+      x: {
+        stacked: true,
+        reverse: true,
+      },
+      y: {
+        stacked: true,
+      },
+    },
+  };
+
+  //[1,2,3,4]구역
+  const labels: number[] = Array.from(
+    { length: sectionPracticeArr.length },
+    (_, i) => i + 1
+  );
+
+  const excellentCount: number[] = labels.map(
+    (i) => sectionPracticeArr[i - 1].poseMessages.Excellent
+  );
+  const greatCount: number[] = labels.map(
+    (i) => sectionPracticeArr[i - 1].poseMessages.Great
+  );
+  const goodCount: number[] = labels.map(
+    (i) => sectionPracticeArr[i - 1].poseMessages.Good
+  );
+  const missCount: number[] = labels.map(
+    (i) => sectionPracticeArr[i - 1].poseMessages.Miss
+  );
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Excellent",
+        data: excellentCount,
+        borderColor: "rgb(54, 162, 235)",
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+      },
+      {
+        label: "Great",
+        data: greatCount,
+        borderColor: "rgb(75, 192, 192)",
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+      },
+      {
+        label: "Good",
+        data: goodCount,
+        borderColor: "rgb(255, 159, 64)",
+        backgroundColor: "rgba(255, 159, 64, 0.2)",
+      },
+      {
+        label: "Miss",
+        data: missCount,
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+      },
+    ],
+  };
+
+  return (
+    <>
+      {sectionPracticeArr.length > 0 && <Bar options={options} data={data} />}
+    </>
+  );
+}
