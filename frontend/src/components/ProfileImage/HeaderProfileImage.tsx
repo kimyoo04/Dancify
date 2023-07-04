d96cf04978a5e329ca730a06549c392fc9275f92
Kim <58503130+kimyoo04@users.noreply.github.com>
2023-07-04 13:15:39 +0900

    fix: ProfileImage null 에러 해결

--- frontend/src/components/ProfileImage/HeaderProfileImage.tsx
+++ frontend/src/components/ProfileImage/HeaderProfileImage.tsx
@@ -4,13 +4,19 @@ import Image from "next/image";
 export default function HeaderProfileImage({ imageUrl }: { imageUrl: string }) {
   return (
     <Avatar className="h-12 w-12">
-      <Image
+      {imageUrl ? (<Image
         src={imageUrl}
         alt="profile_image"
         className="object-cover"
         quality={10}
         fill
-      />
+      />) : (<Image
+        src="/images/avatar.png"
+        alt="profile_image"
+        className="object-cover"
+        quality={10}
+        fill
+      />)}
     </Avatar>
   );
 }
