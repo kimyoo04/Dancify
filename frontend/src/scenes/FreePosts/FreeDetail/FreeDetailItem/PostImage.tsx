89dcdbde6bf68ac034178f2ddb7a2b9ae40754dc
Kim Yoo <kimyoo04eco@naver.com>
2023-07-05 20:48:45 +0900

    css: 자유 게시글의 이미지 height를 반응형으로 수정

--- frontend/src/scenes/FreePosts/FreeDetail/FreeDetailItem/PostImage.tsx
+++ frontend/src/scenes/FreePosts/FreeDetail/FreeDetailItem/PostImage.tsx
@@ -8,7 +8,7 @@ export default function PostImage({ src }: PostImageProps) {
   return (
     <>
       {src !== "" && src !== null && (
-        <div className="relative h-96 w-full overflow-hidden">
+        <div className="relative h-96 w-full overflow-hidden md:h-[500px] lg:h-[700px]">
           <Image
             src={src}
             alt={"post_image"}
