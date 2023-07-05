17a519c903fb18c4487c3e28b605548d36104b10
Kim Yoo <kimyoo04eco@naver.com>
2023-07-05 16:24:22 +0900

    fix: 다음 구간 영상의 데이터가 포함 안되는 문제 수정

--- frontend/src/scenes/Practice/Play/SectionPlay/index.tsx
+++ frontend/src/scenes/Practice/Play/SectionPlay/index.tsx
@@ -75,9 +75,11 @@ export default function SectionPlay({
   }, [playIndex, data.sections, sectionIndex]);
 
   // 연습 모드 or 실전 모드 구분 후 선택된 섹션의 url 배열 가져오기
-  const selectedSectionsData = isRealMode
-    ? [data.sections[0]]
-    : data.sections.filter((section) => section.sectionId === sectionId);
+const selectedSectionsData = isRealMode
+  ? [data.sections[0]]
+  : data.sections.filter((section) =>
+      selectedSections.find((item) => item.sectionId === section.sectionId)
+    );
 
   // 동영상 및 진행 바 관련
   const playerRef = useRef<ReactPlayer>(null);
