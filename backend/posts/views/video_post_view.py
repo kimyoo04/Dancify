cc328570c256271c652fffd14d3d4cafcb7bdd62
wath1457 <wath1457@gmail.com>
2023-07-05 19:40:01 +0900

    feat: 자랑게시판 s3 파일삭제 구현

--- backend/posts/views/video_post_view.py
+++ backend/posts/views/video_post_view.py
@@ -20,6 +20,9 @@ from accounts.authentication import get_user_info_from_token
 from s3_modules.upload import upload_video_with_metadata_to_s3
 
 from search_history.models import SearchHistory
+from s3_modules.authentication import get_s3_client
+
+AWS_DOMAIN = "https://dancify-bucket.s3.ap-northeast-2.amazonaws.com/"
 
 
 class VideoPostViewSet(BasePostViewSet):
@@ -113,4 +116,33 @@ class VideoPostViewSet(BasePostViewSet):
         return Response(status=status.HTTP_200_OK)
 
     def destroy(self, request, *args, **kwargs):
+        post_id = kwargs.get('post_id')
+        post = VideoPost.objects.get(post_id=post_id)
+        video_url = post.video
+
+        # 확장자 .m3u8 제거
+        video_uuid = video_url.split('/')[-1][:-5]
+
+        if video_url is not None:
+            try:
+                # input 버킷의 원본 동영상 삭제
+                s3 = get_s3_client()
+                bucket_name = 'dancify-input'
+                s3_response = s3.list_objects_v2(Bucket=bucket_name)
+
+                for obj in s3_response['Contents']:
+                    if video_uuid in obj['Key']:
+                        s3.delete_object(Bucket=bucket_name, Key=obj['Key'])
+
+                # output 버킷의 스트리밍 파일 삭제
+                bucket_name = 'dancify-output'
+                s3_response = s3.list_objects_v2(Bucket=bucket_name)
+
+                for obj in s3_response['Contents']:
+                    if video_uuid in obj['Key']:
+                        s3.delete_object(Bucket=bucket_name, Key=obj['Key'])
+
+            except Exception as e:
+                print(f's3파일(동영상) 삭제 실패: {e}')
+
         return super().destroy(request, *args, **kwargs)
