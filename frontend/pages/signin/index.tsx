34c445247d4be6ba93456431076f6594efe0d8c1
Kim Yoo <kimyoo04eco@naver.com>
2023-07-06 14:55:23 +0900

    delete: signin page getServerSideProps 제거

--- frontend/pages/signin/index.tsx
+++ frontend/pages/signin/index.tsx
@@ -1,7 +1,5 @@
 import AuthLayout from "@layouts/AuthLayout";
 import SignIn from "@scenes/SignIn";
-import { GetServerSideProps } from "next";
-import { verify } from "jsonwebtoken";
 
 export default function SignInPage() {
   return (
@@ -10,32 +8,3 @@ export default function SignInPage() {
     </AuthLayout>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  try {
-    const { req } = ctx;
-    const token = req.cookies["Access-Token"];
-    const secret = process.env.NEXT_PUBLIC_ENV_JWT_SECRET_KEY;
-    if (token && secret) {
-      verify(token, secret);
-    } else {
-      // 정상
-      return {
-        props: {},
-      };
-    }
-
-    // Home 페이지로 리다이렉트
-    return {
-      redirect: {
-        permanent: false,
-        destination: "/",
-      },
-    };
-  } catch (error) {
-    // 정상
-    return {
-      props: {},
-    };
-  }
-};
