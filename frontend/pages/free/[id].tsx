4e838c95b6a0e557e6655b6ef7871937ab3627d3
Kim Yoo <kimyoo04eco@naver.com>
2023-07-06 14:18:04 +0900

    delete: 자유게시글 상세 페이지 접근 허용 처리

--- frontend/pages/free/[id].tsx
+++ frontend/pages/free/[id].tsx
@@ -1,8 +1,6 @@
 import { useRouter } from "next/router";
 import MainLayout from "@layouts/MainLayout";
 import FreePostDetail from "@scenes/FreePosts/FreeDetail";
-import { GetServerSideProps } from "next";
-import { verify } from "jsonwebtoken";
 
 export default function FreePostDetailPage() {
   const router = useRouter();
@@ -16,35 +14,3 @@ export default function FreePostDetailPage() {
     );
   }
 }
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  try {
-    const { req } = ctx;
-    const token = req.cookies["Access-Token"];
-    const secret = process.env.NEXT_PUBLIC_ENV_JWT_SECRET_KEY;
-    if (token && secret) {
-      verify(token, secret);
-    } else {
-      // Sign In 페이지로 리다이렉트
-      return {
-        redirect: {
-          permanent: false,
-          destination: "/signin",
-        },
-      };
-    }
-
-    // 정상
-    return {
-      props: {},
-    };
-  } catch (error) {
-    return {
-      // Sign In 페이지로 리다이렉트
-      redirect: {
-        permanent: false,
-        destination: "/signin",
-      },
-    };
-  }
-};
