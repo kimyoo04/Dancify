611dc518114c56e0f112e186e7cedaccceeb682c
Kim Yoo <kimyoo04eco@naver.com>
2023-07-10 15:25:55 +0900

    fix: 댄서 게시글 피드백 가격을 10만원 미만으로 설정

--- frontend/src/scenes/Posts/AddPost/AddDancerPostItem/DetailInfo/FeedbackPrice.tsx
+++ frontend/src/scenes/Posts/AddPost/AddDancerPostItem/DetailInfo/FeedbackPrice.tsx
@@ -15,8 +15,8 @@ export default function FeedbackPrice({
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const rawValue = event.target.value;
 
-    // 숫자가 아닌 문자 제거 1,000,000 이상 입력 불가
-    const numericValue = rawValue.replace(/[^0-9]/g, "").slice(0, 6);
+    // 숫자가 아닌 문자 제거 100,000 이상 입력 불가
+    const numericValue = rawValue.replace(/[^0-9]/g, "").slice(0, 5);
 
     // 세 자리 수마다 쉼표 추가 및 화면 출력
     const formattedValue = numericValue.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -41,7 +41,7 @@ export default function FeedbackPrice({
           name="feedbackPrice"
           type="text"
           className="flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-          placeholder="10만원 이하"
+          placeholder="10만원 미만"
           value={feedbackPriceText}
           onChange={handleChange}
         />
